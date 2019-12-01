e7a6ca5e6b41ae274dec28bc521f859c2bef93d1
YellowZaki <algoca99@gmail.com>
2019-12-01 18:39:15 +0100

    Vista graph a Bien

--- models/upocargocliente.py
+++ models/upocargocliente.py
@@ -8,7 +8,7 @@ class upocargocliente(models.Model):
     DNI = fields.Char('DNI', size=9, required=True)
     name = fields.Char('Nombre', size=64, required=True)
     apellidos = fields.Char('Apellidos', size=64, required=True)
-    ciudad = fields.Char('CIudad', size =32, required=True)
+    ciudad = fields.Char('Ciudad', size =32, required=True)
     direccion = fields.Char('Direccion', size=128, required=True)
     avatar = fields.Binary('Avatar')
     upocargomudanza_ids =  fields.One2many('upocargo.upocargomudanza', 'upocargocliente_id', 'Mudanzas')  
