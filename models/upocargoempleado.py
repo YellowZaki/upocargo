2cabdea039404241201f303fba313d6fd3c2cf1b
mmuomar <55514639+mmuomar@users.noreply.github.com>
2020-01-12 01:50:58 +0100

    Eliminados servicios dentro de vista empleado

--- models/upocargoempleado.py
+++ models/upocargoempleado.py
@@ -9,7 +9,6 @@ class upocargoempleado(models.Model):
     name = fields.Char('Nombre', size=64, required=True)
     puesto = fields.Selection([('encargado','Encargado'),('conductor','Conductor'),('operario','Operario')],'Puesto')
     avatar = fields.Binary('Avatar')
-    upocargomudanza_ids =  fields.Many2many('upocargo.upocargomudanza', string='Mudanzas')  
-    upocargoguardamueble_ids =  fields.Many2many('upocargo.upocargoguardamueble', string='Guardamuebles')
+    upocargoservicio_ids =  fields.Many2many('upocargo.upocargoservicio', string='Servicios')  
     
     _sql_constraints = [('upocargo_unique_dni_empleado','UNIQUE (DNI)','El DNI del empleado debe ser único')]
